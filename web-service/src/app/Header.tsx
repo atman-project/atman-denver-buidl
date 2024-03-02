72709de68985188c6881ad748ef0ee2104dc6acb
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2024-03-02 07:02:02 -0700

    change delegatee-client to web-service

--- web-service/src/app/Header.tsx
+++ web-service/src/app/Header.tsx
@@ -3,10 +3,10 @@ import { useWeb3Context } from "../hooks/useWeb3Context";
 
 export function Header() {
   const { connectWallet, account } = useWeb3Context();
-  
+
   return (
     <header>
-      <h1>Delegate</h1>
+      <h1>Web Service</h1>
       <button type="button" onClick={connectWallet}>Connect Wallet</button>
       <p>{account}</p>
     </header>
