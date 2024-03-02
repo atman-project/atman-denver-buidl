65db9b92ee53eaa153ea89a2399deefe6b4c5c49
Jungho Bang <jungho.bang@coinbase.com>
2024-03-01 19:56:14 -0700

    contract rename

--- issue-client/contracts/src/AtmanOnchainSimulator.sol
+++ issue-client/contracts/src/AtmanOnchainSimulator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract AccessControl {
+contract AtmanOnchainSimulator {
     enum Permission { Delegatee, Verifier }
 
     struct PermissionEntry {
